a3f43c644d3911c47716bfda0eadf3edac245f58
Jennifer Dimas <57056027+JJDIMAS@users.noreply.github.com>
2021-01-03 02:30:15 -0600

    Main design

---
+++ SMEEI/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SMEEI
+//
+//  Created by Mac19 on 30/12/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
