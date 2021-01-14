eaf4caf0c2a61dfc47677467bb15d666ef6de42b
JJDIMAS <jennifer.correctivo@gmail.com>
2021-01-13 23:04:46 -0600

    mismatch fixed

--- SMEEI/ViewController.swift
+++ SMEEI/ViewController.swift
@@ -9,17 +9,14 @@ import UIKit
 
 class ViewController: UIViewController {
 
+
     @IBOutlet weak var mailTextField: UITextField!
     @IBOutlet weak var passwordTextField: UITextField!
     @IBOutlet weak var accessButton: UIButton!
-    
+
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
         accessButton.layer.cornerRadius = 20.0
-
+        }
     }
-
-
-}
-
