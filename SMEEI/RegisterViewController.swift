2d627a597abbfaf83b7754fb2a6c3c52d0ec3b89
Jennifer Dimas <jennifer.correctivo@gmail.com>
2021-01-28 04:42:31 -0600

    Firebase authentication added

--- SMEEI/RegisterViewController.swift
+++ SMEEI/RegisterViewController.swift
@@ -6,9 +6,13 @@
 //
 
 import UIKit
+import FirebaseAuth
 
 class RegisterViewController: UIViewController {
 
+    @IBOutlet weak var passwordTextField: UITextField!
+    @IBOutlet weak var mailTextField: UITextField!
+    @IBOutlet weak var nameTextField: UITextField!
     @IBOutlet weak var registerButton: UIButton!
     
     override func viewDidLoad() {
@@ -21,6 +25,38 @@ class RegisterViewController: UIViewController {
     
 
     @IBAction func registerButton(_ sender: UIButton) {
+        if (mailTextField.text == "" || passwordTextField.text == "" || nameTextField.text == ""){
+            alerta(mensaje: "Los campos no pueden estar vacíos")
+        }else{
+            if let email = mailTextField.text, let password = passwordTextField.text{
+                Auth.auth().createUser(withEmail: email, password: password) { authResult, error in
+                    if let e = error{
+                        print(e.localizedDescription)
+                        if(e.localizedDescription.contains("password")){
+                            self.alerta(mensaje: "La contraseña debe tener al menos 6 caracteres")
+                        }else if e.localizedDescription.contains("email") {
+                            self.alerta(mensaje: "Ingresa un correo válido")
+                        }
+                        
+                    }else{
+                        //Ya podemos registrar el usuario
+                        self.performSegue(withIdentifier: "menuPrincipal", sender: self)
+                        
+                    }
+                }
+            }
+        }
+
     }
     
+    func alerta (mensaje : String ){
+        let alerta = UIAlertController(title: "Error", message: "Verifica los datos proporcionados. \n \(mensaje)", preferredStyle: .alert)
+        let accion = UIAlertAction(title: "Aceptar", style: .default, handler: nil)
+        alerta.addAction(accion)
+        present(alerta, animated: true, completion: nil)
+        
+    }
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        self.view.endEditing(true)
+    }
 }
--- SMEEI/ViewController.swift
+++ SMEEI/ViewController.swift
@@ -6,6 +6,8 @@
 //
 
 import UIKit
+import Firebase
+import FirebaseAuth
 
 class ViewController: UIViewController {
 
@@ -18,8 +20,48 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
         accessButton.layer.cornerRadius = 20.0
-        }
-    @IBAction func accessButton(_ sender: UIButton) {
         
+        }
+    func ValidarSesion(){
+        if Auth.auth().currentUser != nil {
+            // User is signed in.
+            performSegue(withIdentifier: "MainToConsult", sender: self)
+        }
+    }
+    
+    @IBAction func accessButton(_ sender: UIButton) {
+        if let email = mailTextField.text , let password = passwordTextField.text {
+                    Auth.auth().signIn(withEmail: email, password: password) { [weak self] authResult, error in
+                           if let e = error {
+                               print(e.localizedDescription)
+                               if email == "" , password == "" {
+                                   self?.alerta(mensaje: "Los campos no pueden estar vacíos")
+                               }
+                               if e.localizedDescription.contains("invalid"){
+                                   self?.alerta(mensaje: "Contraseña incorrecta")
+                               }else if e.localizedDescription.contains("email"){
+                                   self?.alerta(mensaje: "El correo es inválido")
+                               }else if e.localizedDescription.contains("record"){
+                                   self?.alerta(mensaje: "Este usuario no existe")
+                               }
+                           }else{
+                               if let respuestaFirebase = authResult {
+                                   print("\(respuestaFirebase.user) inicio sesión correctamente")
+                                   self?.performSegue(withIdentifier: "MainToConsult", sender: self)
+                               }
+                           }
+                       }
+                   }
+                }
+    
+    func alerta(mensaje : String){
+        let alerta = UIAlertController(title: "Error", message: "Verifica los datos proporcionados. \n \(mensaje)", preferredStyle: .alert)
+        let accion = UIAlertAction(title: "Aceptar", style: .default, handler: nil)
+        alerta.addAction(accion)
+        present(alerta, animated: true, completion: nil)
+    }
+    
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        self.view.endEditing(true)
     }
 }
